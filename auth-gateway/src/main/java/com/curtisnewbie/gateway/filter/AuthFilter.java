45c180292c7c70917c6d182277f6e49b4a535b07
yongj.zhuang <zhuangyongj@gmail.com>
2022-04-29 00:49:36 +0800

    Make it kinda run

--- auth-gateway/src/main/java/com/curtisnewbie/gateway/filter/AuthFilter.java
+++ auth-gateway/src/main/java/com/curtisnewbie/gateway/filter/AuthFilter.java
@@ -3,15 +3,16 @@ package com.curtisnewbie.gateway.filter;
 import com.curtisnewbie.common.util.JsonUtils;
 import com.curtisnewbie.common.vo.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
@@ -43,11 +44,22 @@ public class AuthFilter implements GlobalFilter, Ordered {
         ServerHttpResponse resp = exchange.getResponse();
 
         // extract token from HttpHeaders
-        Optional<String> tokenOpt = getFirstToken(headers);
+        final Optional<String> tokenOpt = getFirstToken(headers);
+        if (!tokenOpt.isPresent()) {
+            writeError("Please login first", resp);
+            return Mono.empty();
+        }
 
-        // token not present or invalid, user should sign-in first
-        if (!tokenOpt.isPresent() || !isTokenValid(tokenOpt.get())) {
-            writeErrorMsg("Please login first", resp);
+        final Mono<Result<String>> resultMono = exchangeToken(tokenOpt.get());
+        final Result<String> result = resultMono.block();
+
+        if (result == null) {
+            writeError("Unexpected Technical Error", resp);
+            return Mono.empty();
+        }
+
+        if (result.hasError()) {
+            write(result, resp);
             return Mono.empty();
         }
 
@@ -56,20 +68,17 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
     /** Check whether this request is a login request */
     private boolean isLoginRequest(final ServerWebExchange exg) {
-        // todo read database probably?
-        return exg.getRequest().getURI().getPath().startsWith("/login");
+        return exg.getRequest().getURI().getPath().equalsIgnoreCase("/auth-service/api/login");
     }
 
-    // todo return a mono
     /** Validate the token */
-    private boolean isTokenValid(final String token) {
-//        buildAuthClient()
-//                .post()
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .body(Mono.just(null), Object.class) // todo
-
-        // todo impl this
-        return false;
+    private Mono<Result<String>> exchangeToken(final String token) {
+        Assert.notNull(token, "token is empty");
+        return WebClient.create().get()
+                .uri("http://auth-service/remote/user/token" + token)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Result<String>>() {
+                });
     }
 
     /** Extract the first token from headers */
@@ -86,25 +95,24 @@ public class AuthFilter implements GlobalFilter, Ordered {
     }
 
     /** Write error message to response */
-    private void writeErrorMsg(final String errMsg, final ServerHttpResponse resp) {
+    private void writeError(final String errMsg, final ServerHttpResponse resp) {
+        write(Result.error(errMsg), resp);
+    }
+
+    /** Write error message to response */
+    private void write(final Result<?> result, final ServerHttpResponse resp) {
         try {
-            final String errorMsg = JsonUtils.writeValueAsString(Result.error(errMsg));
+            resp.setStatusCode(HttpStatus.BAD_REQUEST); // todo which status code do we set?
+            final String errorMsg = JsonUtils.writeValueAsString(result);
             final DataBuffer buf = resp.bufferFactory().wrap(errorMsg.getBytes(StandardCharsets.UTF_8));
             resp.writeWith(Flux.just(buf));
         } catch (IOException e) {
-            throw new IllegalStateException("Failed to deserialize response messgae", e);
+            throw new IllegalStateException("Failed to deserialize response message", e);
         }
     }
 
     @Override
     public int getOrder() {
         return HIGHEST_PRECEDENCE; // first
-    }
-
-    private WebClient buildAuthClient() {
-        // todo endpoint used to validate and exchange token
-        return WebClient.builder()
-                .baseUrl("http://auth-service/")
-                .build();
     }
 }
