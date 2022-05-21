ecdd066a2b42594f5b186a3bf4d1ff1b580392d5
yongj.zhuang <zhuangyongj@gmail.com>
2022-05-21 13:53:06 +0800

    Modified: deploy/deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,4 +4,4 @@ jarname="auth-gateway-build.jar"
 
 mvn clean package -f ../auth-gateway/pom.xml -Dmaven.test.skip=true
 
-scp "../auth-gateway/target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-gateway/auth-gateway.jar"
+scp "../auth-gateway/target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-gateway/build/auth-gateway.jar"
