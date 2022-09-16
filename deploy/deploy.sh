eff841caf7a2a959777b54cb41326ec9c1bfa15a
yongj.zhuang <zhuangyongj@gmail.com>
2022-09-16 23:44:27 +0800

    Modified:  auth-gateway/src/main/resources/bootstrap-test.yml Modified:  deploy/deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,13 @@
 jarname="auth-gateway-build.jar"
 
 mvn clean package -f ../auth-gateway/pom.xml -Dmaven.test.skip=true
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
 
 scp "../auth-gateway/target/${jarname}" "alphaboi@curtisnewbie.com:~/services/auth-gateway/build/auth-gateway.jar"
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+
+ssh  "alphaboi@curtisnewbie.com" "cd services; docker-compose up -d --build auth-gateway"
